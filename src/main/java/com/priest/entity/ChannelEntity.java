d3c250af827da67e0486e56024cc5c69b520f1b5
pirest89 <thang89@gmail.com>
2018-12-06 09:01:02 +0700

    add videoservice

--- src/main/java/com/priest/entity/ChannelEntity.java
+++ src/main/java/com/priest/entity/ChannelEntity.java
@@ -1,5 +1,52 @@
 package com.priest.entity;
 
-public class ChannelEntity {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "channel")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChannelEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "description")
+	private String description;
+
+	@Id
+	private Long id;
+
+	@Column(name = "channel_guid")
+	private String channelGuid;
+
+	@Column(name = "image_url")
+	private String imageUrl;
+
+	@Column(name = "status")
+	private String status;
+
+	@Column(name = "created_date")
+	private String createdDate;
+
+	@Column(name = "lastModified")
+	private String lastModified;
 
 }
---
+++ src/main/java/com/priest/service/VideoService.java
@@ -0,0 +1,5 @@
+package com.priest.service;
+
+public class VideoService {
+
+}
